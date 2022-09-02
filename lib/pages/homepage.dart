68be32a27eb9584313320a9d74f047087d80b77e
songsangmin <qjsrotkdals@gmail.com>
2022-09-02 23:48:36 +0900

    [헤리티지] Home 임시 UI

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -12,7 +12,22 @@ class HomePage extends StatefulWidget{
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 150,
+              child: Image.asset("assets/logo.png",),)
+            ],
+          ),
+        )
+      ),
+    );
   }
 
 }
