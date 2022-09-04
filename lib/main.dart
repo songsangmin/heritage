a407d7be0964593a87a75292fbbc56ea15fca078
songsangmin <qjsrotkdals@gmail.com>
2022-09-04 23:55:22 +0900

    [헤리티지] Firebase 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heritage/pages/homepage.dart';
 import 'package:heritage/pages/loginpage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+
   runApp(MyApp());
 }
 
