d8f507653440afc7718a3483070b12dafb96ad1d
songsangmin <qjsrotkdals@gmail.com>
2022-09-05 23:42:27 +0900

    [헤리티지] Auth FireStore 연동 확인

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../models/user_model.dart';
 import 'loginpage.dart';
 
 class HomePage extends StatefulWidget{
@@ -10,6 +13,24 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePage extends State<HomePage> {
+
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loginUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore
+        .instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value){
+          this.loginUser = UserModel.fromMap(value.data());
+          setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +43,20 @@ class _HomePage extends State<HomePage> {
             children: <Widget>[
               SizedBox(
                 height: 150,
-              child: Image.asset("assets/logo.png",),)
+              child: Image.asset("assets/logo.png",),),
+              Text("${loginUser.firstName} + ${loginUser.secondName}"),
+              Text("${loginUser.email}"),
             ],
           ),
         )
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
   }
 
 }
