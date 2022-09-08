b5935f4a09a5d192e9c5bd43e9a4cfcdaa5bd720
songsangmin <qjsrotkdals@gmail.com>
2022-09-08 21:00:18 +0900

    [헤리티지] BoardPage 생성

---
+++ lib/pages/board_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Boardpage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _BoardPage();
+  }
+
+}
+
+class _BoardPage extends State<Boardpage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
